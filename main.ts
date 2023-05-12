dc790535b9ed0eb8171a58ff211850abae8be81a
juju2303 <132678792+juju2303@users.noreply.github.com>
2023-05-12 16:52:09 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,38 @@
+basic.pause(1000)
+for (let index = 0; index < 4; index++) {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(200)
+}
 basic.forever(function () {
-	
+    serial.writeLine("température")
+    serial.writeNumber(input.temperature())
+    serial.writeLine("")
+    serial.writeLine("Intensité lumineuse")
+    serial.writeNumber(input.lightLevel())
+    serial.writeLine("")
+    serial.writeLine("niveau sonore")
+    serial.writeNumber(input.soundLevel())
+    serial.writeLine("")
 })
